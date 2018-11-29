bb482db98cdfc9af0f814dbab7aee9dc541a7906
lizg <lnatjysys@163.com>
2018-11-29 13:32:39 +0800

    1

--- src/main/java/com/hzkans/crm/modules/trade/web/OrderController.java
+++ src/main/java/com/hzkans/crm/modules/trade/web/OrderController.java
@@ -1,13 +1,11 @@
 
 package com.hzkans.crm.modules.trade.web;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.hzkans.crm.common.config.Global;
 import com.hzkans.crm.common.persistence.Page;
 import com.hzkans.crm.common.utils.StringUtils;
 import com.hzkans.crm.common.web.BaseController;
+import com.hzkans.crm.modules.trade.entity.Order;
+import com.hzkans.crm.modules.trade.service.OrderService;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,9 +13,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import com.hzkans.crm.modules.trade.entity.Order;
-import com.hzkans.crm.modules.trade.service.OrderService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 订单主表Controller
@@ -25,17 +23,17 @@ import com.hzkans.crm.modules.trade.service.OrderService;
  * @version 2018-11-27
  */
 @Controller
-@RequestMapping(value = "${adminPath}/trade/tOrder")
+@RequestMapping(value = "${adminPath}/trade/order")
 public class OrderController extends BaseController {
 
 	@Autowired
-	private OrderService tOrderService;
+	private OrderService orderService;
 	
 	@ModelAttribute
 	public Order get(@RequestParam(required=false) String id) {
 		Order entity = null;
 		if (StringUtils.isNotBlank(id)){
-			entity = tOrderService.get(id);
+			entity = orderService.get(id);
 		}
 		if (entity == null){
 			entity = new Order();
@@ -46,7 +44,7 @@ public class OrderController extends BaseController {
 	@RequiresPermissions("trade:tOrder:view")
 	@RequestMapping(value = {"list", ""})
 	public String list(Order tOrder, HttpServletRequest request, HttpServletResponse response, Model model) {
-		Page<Order> page = tOrderService.findPage(new Page<Order>(request, response), tOrder);
+		Page<Order> page = orderService.findPage(new Page<Order>(request, response), tOrder);
 		model.addAttribute("page", page);
 		return "modules/trade/orderList";
 	}
@@ -56,25 +54,11 @@ public class OrderController extends BaseController {
 	public String form(Order tOrder, Model model) {
 		model.addAttribute("tOrder", tOrder);
 		return "modules/trade/orderForm";
+
+
+
+
 	}
 
-	@RequiresPermissions("trade:tOrder:edit")
-	@RequestMapping(value = "save")
-	public String save(Order tOrder, Model model, RedirectAttributes redirectAttributes) {
-		if (!beanValidator(model, tOrder)){
-			return form(tOrder, model);
-		}
-		tOrderService.save(tOrder);
-		addMessage(redirectAttributes, "保存订单成功");
-		return "redirect:"+ Global.getAdminPath()+"/trade/tOrder/?repage";
-	}
-	
-	@RequiresPermissions("trade:tOrder:edit")
-	@RequestMapping(value = "delete")
-	public String delete(Order tOrder, RedirectAttributes redirectAttributes) {
-		tOrderService.delete(tOrder);
-		addMessage(redirectAttributes, "删除订单成功");
-		return "redirect:"+Global.getAdminPath()+"/trade/tOrder/?repage";
-	}
 
 }
