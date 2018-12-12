4bf1f04c1793d6d11d53db743c48108652695372
dtm <2732589165@qq.com>
2018-12-12 15:18:54 +0800

    自动回复

--- src/main/java/com/hzkans/crm/modules/wechat/web/WechatReplyController.java
+++ src/main/java/com/hzkans/crm/modules/wechat/web/WechatReplyController.java
@@ -19,7 +19,6 @@ import com.hzkans.crm.modules.sys.utils.UserUtils;
 import com.hzkans.crm.modules.wechat.constants.WechatErrorEnum;
 import com.hzkans.crm.modules.wechat.entity.*;
 import com.hzkans.crm.modules.wechat.service.NewWechatReplyService;
-import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -29,9 +28,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.hzkans.crm.modules.wechat.service.WechatReplyService;
 
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
+
 
 /**
  * 微信公众号自动回复Controller
