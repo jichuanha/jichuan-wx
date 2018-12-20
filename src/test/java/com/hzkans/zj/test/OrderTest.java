297a2a03c3bf8b31081c30742f21a16151b69d1b
lizg <lnatjysys@163.com>
2018-12-20 17:56:37 +0800

    代码合并

--- src/test/java/com/hzkans/zj/test/OrderTest.java
+++ src/test/java/com/hzkans/zj/test/OrderTest.java
@@ -5,23 +5,16 @@ import com.hzkans.crm.modules.trade.entity.Order;
 import com.hzkans.crm.modules.trade.service.OrderService;
 import org.junit.Test;
 
+public class OrderTest extends BaseTest {
 
-public class OrderTest extends BaseTest{
+    @Test
+    public void getOrder() throws Exception {
+        OrderService orderService = context.getBean(OrderService.class);
 
+        Order orderDO = new Order();
+        orderDO.setOrderSn("272200608488052227");
+        Order order = orderService.getOrder(orderDO);
+        System.out.println("order:{}" + JsonUtil.toJson(order));
 
-	@Test
-	public void getOrder ()throws Exception{
-		OrderService orderService = context.getBean(OrderService.class);
-
-		Order orderDO = new Order();
-		orderDO.setOrderSn("272200608488052227");
-		Order order =  orderService.getOrder(orderDO);
-		System.out.println("order:{}"+ JsonUtil.toJson(order));
-
-
-
-	}
-	public static void main(String[] args) {
-		
-	}
+    }
 }
